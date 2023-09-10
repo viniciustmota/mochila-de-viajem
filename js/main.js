d0e8d3f51336beee0b1fce6c65c97b86a633a51b
unknown <motaviny140@gmail.com>
2023-09-10 00:41:21 -0300

    Salvando dados no localStorage

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 const form = document.querySelector("#novoItem");
 const lista = document.querySelector("#lista");
+const itens = []
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
 
-  criaElemento(
-    evento.target.elements["nome"].value,
-    evento.target.elements["quantidade"].value
-  );
+  const nome = evento.target.elements["nome"];
+  const quantidade = evento.target.elements["quantidade"];
+  criaElemento(nome.value, quantidade.value);
+
+  nome.value = "";
+  quantidade.value = "";
 });
 
 function criaElemento(nome, quantidade) {
@@ -21,4 +24,13 @@ function criaElemento(nome, quantidade) {
   novoItem.innerHTML += nome;
 
   lista.appendChild(novoItem);
+
+  const itemAtual = {
+    nome: nome,
+    quantidade: quantidade,
+  };
+
+itens.push(itemAtual)
+
+  localStorage.setItem("item", JSON.stringify(itens));
 }
