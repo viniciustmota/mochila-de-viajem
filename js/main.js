1db59d8c9bc60134b0ccca4ab55fc05822abf38f
unknown <motaviny140@gmail.com>
2023-09-10 10:11:05 -0300

    Criando li com dados salvos no localStorage

--- js/main.js
+++ js/main.js
@@ -1,36 +1,45 @@
 const form = document.querySelector("#novoItem");
 const lista = document.querySelector("#lista");
-const itens = []
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+itens.forEach((elemento) => {
+  criaElemento(elemento);
+});
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
 
   const nome = evento.target.elements["nome"];
   const quantidade = evento.target.elements["quantidade"];
-  criaElemento(nome.value, quantidade.value);
+
+  const itemAtual = {
+    nome: nome.value,
+    quantidade: quantidade.value,
+  };
+
+  if(nome.value != "" && quantidade.value != ""){
+  criaElemento(itemAtual);
+  }else {
+    alert("Insira o nome e a quantidade do item");
+  }
+
+  itens.push(itemAtual);
+
+  localStorage.setItem("itens", JSON.stringify(itens));
 
   nome.value = "";
   quantidade.value = "";
 });
 
-function criaElemento(nome, quantidade) {
-  const novoItem = document.createElement("li");
-  novoItem.classList.add("item");
+function criaElemento(item) {
+    const novoItem = document.createElement("li");
+    novoItem.classList.add("item");
 
-  const numeroItem = document.createElement("strong");
-  numeroItem.innerHTML = quantidade;
+    const numeroItem = document.createElement("strong");
+    numeroItem.innerHTML = item.quantidade;
 
-  novoItem.appendChild(numeroItem);
-  novoItem.innerHTML += nome;
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += item.nome;
 
-  lista.appendChild(novoItem);
-
-  const itemAtual = {
-    nome: nome,
-    quantidade: quantidade,
-  };
-
-itens.push(itemAtual)
-
-  localStorage.setItem("item", JSON.stringify(itens));
+    lista.appendChild(novoItem);
 }
